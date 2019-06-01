26256f4d51d6fe647945569976e53dca5f308454
toshihiro kokubun <dev.kokubun@gmail.com>
2019-06-01 22:02:09 +0900

    modified:   medicine/post.php

--- medicine/post.php
+++ medicine/post.php
@@ -3,6 +3,8 @@
   $t = filter_input('t', 'GET');
   $u = filter_input('u', 'GET');
 
+  $test = $_GET['t'];
+
 ?>
 
 <!doctype html>
@@ -42,7 +44,8 @@
         </div>
       </div>
     </div>
-    <input type="hidden" name="t" id="t" value="<?php echo ($t); ?>">
+    <input type="hidden" name="t" id="t" value="<?php echo htmlspecialchars($t); ?>">
+    <input type="hidden" name="test" id="test" value="<?php echo htmlspecialchars($test); ?>">
     
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
