25930c0b436c0fe83b970f0851497dc37d1bcd4d
toshihiro kokubun <dev.kokubun@gmail.com>
2019-06-01 21:52:14 +0900

    modified:   medicine/post.php

--- medicine/post.php
+++ medicine/post.php
@@ -48,6 +48,7 @@
         foreach ($u as $value) {
           echo $value;
         }
+        echo (1);
       }
     ?>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
