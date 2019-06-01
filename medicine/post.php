4d257e2e69430cabe245f5a524b3619fcded435c
toshihiro kokubun <dev.kokubun@gmail.com>
2019-06-01 20:01:55 +0900

    modified:   post.php

--- medicine/post.php
+++ medicine/post.php
@@ -11,7 +11,7 @@
       <h2>お薬手帳アプリ</h2>
       <div class="jumbotron">
         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
-          ココを押すと表示
+          お薬記録登録
         </button>
       </div>
     </div>
@@ -29,14 +29,14 @@
             <p>お薬を飲みましたか？</p>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
             <button type="button" class="btn btn-primary">飲みました。</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
           </div>
         </div>
       </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
