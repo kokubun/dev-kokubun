1dc71208b550d2040fbd21043fd088a2bd7f2cb0
toshihiro kokubun <dev.kokubun@gmail.com>
2019-06-01 22:08:57 +0900

    modified:   medicine/post.php

--- medicine/post.php
+++ medicine/post.php
@@ -1,9 +1,7 @@
 <?php
 
-  $t = filter_input('t', 'GET');
-  $u = filter_input('u', 'GET');
-
-  $test = $_GET['t'];
+  $t = filter_input(INPUT_GET, 't');
+  $u = filter_input(INPUT_GET, 'u');
 
 ?>
 
@@ -45,7 +43,13 @@
       </div>
     </div>
     <input type="hidden" name="t" id="t" value="<?php echo htmlspecialchars($t); ?>">
-    <input type="hidden" name="test" id="test" value="<?php echo htmlspecialchars($test); ?>">
+    <?php
+      if ($u) {
+        foreach ($u as $key => $value) {
+          echo '<input type="hidden" name="u[]" class="u" value="'. htmlspecialchars($value) .'">';
+        }
+      }
+    ?>
     
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
