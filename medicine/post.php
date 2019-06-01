2542be11258974729c1c0fea9375487d05feab46
toshihiro kokubun <dev.kokubun@gmail.com>
2019-06-01 19:59:23 +0900

    modified:   post.php

--- medicine/post.php
+++ medicine/post.php
@@ -1,43 +1,40 @@
 <!doctype html>
 <html lang="ja">
   <head>
-    <title>お薬手帳</title>
-  <!-- 必要なメタタグ -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+   <title>お薬手帳</title>
+  </head>
   <body>
-  <div class="container" style="padding-top: 50px;">
-    <h2>お薬手帳アプリ</h2>
-
-    <div class="jumbotron">
-      <p><a class="btn btn-primary btn-lg">登録</a></p>
-      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
-        ココを押すと表示
-      </button>
-    </div>
-  </div>
-  <!-- モーダルの設定 -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">モーダルのタイトル</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
-          <span aria-hidden="true">&times;</span>
+    <div class="container" style="padding-top: 50px;">
+      <h2>お薬手帳アプリ</h2>
+      <div class="jumbotron">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+          ココを押すと表示
         </button>
       </div>
-      <div class="modal-body">
-        <p>モーダルのコンテンツ文。</p>
+    </div>
+    <!-- モーダルの設定 -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">お薬記録登録</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <p>お薬を飲みましたか？</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+            <button type="button" class="btn btn-primary">飲みました。</button>
+          </div>
+        </div>
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
-        <button type="button" class="btn btn-primary">変更を保存</button>
-      </div><!-- /.modal-footer -->
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
+    </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
