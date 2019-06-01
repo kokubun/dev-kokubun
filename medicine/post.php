95839c283c54fff414c3f4d0bf9152ea349440f3
toshihiro kokubun <dev.kokubun@gmail.com>
2019-06-01 19:46:16 +0900

    modified:   post.php

--- medicine/post.php
+++ medicine/post.php
@@ -9,12 +9,10 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-    <h1>こんにちは!</h1>
-    <div class="feature-list">
-      <div class="feature-item">…</div>
-      <div class="feature-item">…</div>
-      <div class="feature-item">…</div>
-    </div>
+  <div class="container" style="padding-top: 50px;">
+    <h2>お薬手帳アプリ</h2>
+    <p><a class="btn btn-primary btn-lg">Learn more</a></p>
+  </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM crossorigin="anonymous"></script>
