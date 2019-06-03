46bf15e9df3c2e787010df6c748c601f1d28a64f
toshihiro kokubun <dev.kokubun@gmail.com>
2019-06-03 23:37:01 +0900

    modified:   serviceworker.js

--- medicine/serviceworker.js
+++ medicine/serviceworker.js
@@ -1,1 +1,27 @@
-self.addEventListener('fetch', function(){});
+var CACHE_NAME  = "cache-v1";
+var urlsToCache = [
+    "https://dev-kokubun.herokuapp.com/medicine/post.php"
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(
+            function(cache){
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(
+        function (response) {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
