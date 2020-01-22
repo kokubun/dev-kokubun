986293a55d8138fbb99f3c898b9a1c0a7795d9a0
kokubun <dev-kokubun@gmail.com>
2020-01-22 23:26:47 +0900

    test

--- test.php
+++ test.php
@@ -1,35 +1,39 @@
 <?php
 
-$host = filter_input(INPUT_GET, 'hs');
-$port = filter_input(INPUT_GET, 'pr');
-$name = filter_input(INPUT_GET, 'nm');
-$user = filter_input(INPUT_GET, 'us');
-$pass = filter_input(INPUT_GET, 'ph');
+$dbUrl = parse_url(getenv('DATABASE_URL'));
 
-try {
-	// DB接続
-	$dbh = new PDO("pgsql:host=$host;port=$port;dbname=$name;user=$user;password=$pass");
-	print("接続成功".'<br>');
-  
-	// SQL作成
-	$sql = 'select * from medicine';
-  
-	//SQL実行
-	foreach ($dbh->query($sql) as $row) {
-		// 指定Columnを一覧表示
-		print_r($row);
-		// print($row['ColumnName'].'<br>');
-	}
+print_r($dbUrl);
 
-	$dbh->beginTransaction();
-	$sql = "insert into medicine values (current_timestamp)";
-	$dbh->exec($sql);
-	$dbh->commit();
+// $host = filter_input(INPUT_GET, 'hs');
+// $port = filter_input(INPUT_GET, 'pr');
+// $name = filter_input(INPUT_GET, 'nm');
+// $user = filter_input(INPUT_GET, 'us');
+// $pass = filter_input(INPUT_GET, 'ph');
+
+// try {
+// 	// DB接続
+// 	$dbh = new PDO("pgsql:host=$host;port=$port;dbname=$name;user=$user;password=$pass");
+// 	print("接続成功".'<br>');
   
-} catch(PDOException $e) {
-	print("接続失敗".'<br>');
-	print($e.'<br>');
-	die();
-}
+// 	// SQL作成
+// 	$sql = 'select * from medicine';
+  
+// 	//SQL実行
+// 	foreach ($dbh->query($sql) as $row) {
+// 		// 指定Columnを一覧表示
+// 		print_r($row);
+// 		// print($row['ColumnName'].'<br>');
+// 	}
+
+// 	$dbh->beginTransaction();
+// 	$sql = "insert into medicine values (current_timestamp)";
+// 	$dbh->exec($sql);
+// 	$dbh->commit();
+  
+// } catch(PDOException $e) {
+// 	print("接続失敗".'<br>');
+// 	print($e.'<br>');
+// 	die();
+// }
 
 exit();
