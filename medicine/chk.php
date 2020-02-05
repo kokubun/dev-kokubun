0287eb42461c66719c707f69808c6e3ea80b5742
kokubun <dev-kokubun@gmail.com>
2020-02-05 22:55:51 +0900

    check

--- medicine/chk.php
+++ medicine/chk.php
@@ -6,11 +6,13 @@ include_once './db.inc';
 
 $LINE_ID = getenv('LINE_ID_1');
 $now_microtime = time();
-$now_datetime = date('YmdHis', strtotime($now_microtime));
+$now_datetime = date('YmdHis', $now_microtime);
+$now_hour = date('H', $now_microtime);
 
 
 echo "check!!!\n";
 echo "{$now_datetime}\n";
+echo "{$now_hour}\n";
 
 
 exit();
