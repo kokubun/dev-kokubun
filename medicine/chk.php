8b2f1e2c4bf8967d00bd77ba5d3a0b790fb0bf97
kokubun <dev-kokubun@gmail.com>
2020-02-13 23:13:47 +0900

    test

--- medicine/chk.php
+++ medicine/chk.php
@@ -4,15 +4,59 @@
 
 include_once './db.inc';
 
+date_default_timezone_set("Asia/Tokyo");
+
+const NOTHING_STATUS	= 0;
+const MORNING_STATUS	= 1;
+const NIGHT_STATUS		= 2;
+
 $LINE_ID = getenv('LINE_ID_1');
 $now_microtime = time();
 $now_datetime = date('YmdHis', $now_microtime);
+$now_day = date('d', $now_microtime);
 $now_hour = date('H', $now_microtime);
 
+echo "check!!!<br>";
+echo "{$now_datetime}<br>";
+echo "{$now_hour}<br>";
 
-echo "check!!!\n";
-echo "{$now_datetime}\n";
-echo "{$now_hour}\n";
+
+// TODO: 毎月1日にデータ削除
+
+// 
+$time_status = chkTimeStatus($now_hour);
+if ($time_status === NOTHING_STATUS) {
+	exit();
+}
+
 
 
 exit();
+
+
+function chkTimeStatus($hour) {
+	$res = NOTHING_STATUS;
+	switch ($hour) {
+		case 0:
+		case 1:
+		case 2:
+		case 22:
+		case 23:
+		case 24:
+			$res = NIGHT_STATUS;
+			break;
+		case 9:
+		case 10:
+		case 11:
+		case 12:
+		case 13:
+		case 14:
+			$res = MORNING_STATUS;
+			break;
+		default:
+			break;
+	}
+
+	return $res;
+	
+}
